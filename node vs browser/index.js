761986fdcc2b85698e52a13920e25aff3f1107cd
userisash <121009931+userisash@users.noreply.github.com>
2023-03-15 11:02:11 +0200

    Add files via upload

---
+++ node vs browser/index.js
@@ -0,0 +1,22 @@
+// Three things Node.js has that client-side JavaScript doesn't have
+
+//1- Access to the file system: Node.js provides APIs to interact with the file system on the server-side
+
+//2- Ability to perform server-side operations: Node.js is used to build server-side applications that can perform various operations like file processing,
+// database operations, sending emails, and more. Client-side JavaScript runs on the user's browser
+// and is limited to manipulating the DOM and making HTTP requests.
+
+//3- Support for real-time applications: Node.js provides support for real-time applications using technologies like WebSockets and socket.io. 
+//This feature is not available in client-side JavaScript
+
+//Three things client-side JavaScript has that Node.js doesn't have are:
+
+//1- Access to the browser's Document Object Model (DOM): Client-side JavaScript runs within the context of a web browser 
+//and has access to the browser's DOM. This allows client-side JavaScript 
+//to manipulate the content and appearance of web pages in real-time.
+
+//2- Access to browser events: Client-side JavaScript can respond to user interactions like clicks, keypresses, and mouse movements. 
+//This feature allows for interactive and responsive user interfaces.
+
+//3- Direct access to client resources: Client-side JavaScript can access the client's resources, such as local storage and cookies. 
+
